524af0d3ff35a6eaeb8f71bf729342a104ef8284
noah lozevski <55212226+noahlozevski@users.noreply.github.com>
2020-09-26 20:45:01 -0400

    first

---
+++ index.js
@@ -0,0 +1,91 @@
+const Discord = require('discord.js')
+const fs = require('fs')
+const _ = require('lodash')
+const { resolve } = require('path')
+const util = require('util')
+
+const bot = new Discord.Client({
+  disableEveryone: true,
+  disabledEvents: ['TYPING_START']
+})
+
+const TOKEN = 'NzU5NTQzMjk3NDgzMDc5NzEy.X2_B0Q.C5um6LycD9SeYC7mtmqN_eUjNps'
+
+bot.login(TOKEN)
+const readFile = util.promisify(fs.readFile)
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+  // console.log(bot)
+})
+
+console.log('listening for messages')
+
+/** channels in the chat */
+const channels = {
+  'general-millis': 'general',
+  'bot-shit': 'bot',
+  'rules': 'rules',
+  'dank-maymays': 'dank',
+  'boomers-only': 'boomers'
+}
+
+/** returns parsed message */
+const msgParse = (msg) => {
+  return {
+    data: {
+      isBot: msg.author.bot,
+      user: msg.author.username,
+      msg: msg.content,
+      channel: msg.channel.name,
+    },
+    shit: msg
+  }
+}
+
+const canIBeAdmin = (msg) => {
+  const words = 'can i be admin'
+  return msg.includes(words)
+}
+
+
+const auto_reply = async (msg) => {
+  let data = await update_words() // grab new word mapping of responses
+  Object.assign(replies, JSON.parse(data))
+  if (msg.data.msg.toLowerCase().includes('add')) {
+    // add this to the mapping
+    let cleaned = msg.data.msg.toLowerCase().replace('add', '').split(',')
+    // if ()
+    console.log('cleaned', cleaned)
+    if (cleaned.length >= 2) {
+      let command = cleaned[0].trim()
+      let response = cleaned[1].trim()
+      Object.assign(replies, { [command]: response })
+      console.log('added new command ', { [command]: response })
+      console.log('current replies', _.entries(replies))
+      return
+    }
+  }
+  console.log('current replies', _.entries(replies))
+  if (_.some(_.keys(replies), k => msg.data.msg.toLowerCase().includes(k)) && _.find(_.keys(replies), (k) => msg.data.msg.includes(k))) {
+    // use the programmed reply
+
+    console.log(_.find(_.keys(replies), (k, v) => msg.data.msg.includes(k)))
+    msg.shit.reply(replies[_.find(_.keys(replies), (k, v) => msg.data.msg.includes(k))])
+  }
+
+}
+
+let replies = {}
+const update_words = async () => {
+  return readFile('update.json', 'utf8')
+}
+
+
+bot.on('message', msg => {
+  const message = msgParse(msg)
+  console.log('message recieved: \n', message.data)
+  if (message.data.isBot) return
+  auto_reply(message)
+
+})
